eaaf8819bbad7388ff559cd2dd74c370b4a71752
Madelineqt <afbritezr41@alumnes.santjosepobrer.es>
2020-02-12 13:54:05 +0100

    perfil arreglafo
    uwu

---
+++ build/precache-manifest.530c8ba1c9ac49d9632aa7827232e2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d52f0dd71b777ffb99d92552a8550c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbacc00797015ffb2025",
+    "url": "/static/css/main.64835c61.chunk.css"
+  },
+  {
+    "revision": "a33afa6c67f4457a250e",
+    "url": "/static/js/2.59350ddd.chunk.js"
+  },
+  {
+    "revision": "fbacc00797015ffb2025",
+    "url": "/static/js/main.041b95fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8049109ac3c6f88d00285d57436b3bda",
+    "url": "/static/media/signup.8049109a.png"
+  }
+]);
