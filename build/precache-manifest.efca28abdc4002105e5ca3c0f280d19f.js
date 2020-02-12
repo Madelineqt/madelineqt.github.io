9fe73715a95d6b97bedc5cb6b848719bcb5897bc
Madelineqt <afbritezr41@alumnes.santjosepobrer.es>
2020-02-12 14:08:14 +0100

    commit url not well put

---
+++ build/precache-manifest.efca28abdc4002105e5ca3c0f280d19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79459e0d92f30f997c4f4e75734fb8e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dcba9c87dbe0cd9c304",
+    "url": "/static/css/main.64835c61.chunk.css"
+  },
+  {
+    "revision": "a33afa6c67f4457a250e",
+    "url": "/static/js/2.59350ddd.chunk.js"
+  },
+  {
+    "revision": "4dcba9c87dbe0cd9c304",
+    "url": "/static/js/main.8fcb52c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8049109ac3c6f88d00285d57436b3bda",
+    "url": "/static/media/signup.8049109a.png"
+  }
+]);
